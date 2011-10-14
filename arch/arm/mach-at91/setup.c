c84e0f1460d97929e33d8dbb1c356de369987ebb
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-10-14 15:17:18 +0800

    ARM: at91: add map_register entry point to soc setup
    this will allow to ioremap the register of the PIT, PMC and others
    and make the code soc independant
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- arch/arm/mach-at91/setup.c
+++ arch/arm/mach-at91/setup.c
@@ -287,6 +287,8 @@ void __init at91_map_io(void)
 
 void __init at91_initialize(unsigned long main_clock)
 {
+	at91_boot_soc.map_register();
+
 	/* Init clock subsystem */
 	at91_clock_init(main_clock);
 
