2aa167fe5b1a8d3a410cb95853eeae2246da1d29
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-10-25 13:02:51 +0800

    at91/board-dt: add pinctrl support
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- arch/arm/mach-at91/board-dt.c
+++ arch/arm/mach-at91/board-dt.c
@@ -30,14 +30,61 @@
 #include <asm/mach/map.h>
 #include <asm/mach/irq.h>
 
+#include <linux/pinctrl/machine.h>
+#include <linux/pinctrl/pinmux.h>
+
 #include "sam9_smc.h"
 #include "generic.h"
 
+/* Pinmux settings */
+static struct pinmux_map usb_a9g20_pinmux_map[] = {
+	PINMUX_MAP_PRIMARY_SYS_HOG("DGBU_RX", "dbgu_rxd", NULL),
+	PINMUX_MAP_PRIMARY_SYS_HOG("DGBU_TX", "dbgu_txd", NULL),
+};
+
+static struct pinmux_map sam9m10g45ek_pinmux_map[] = {
+	PINMUX_MAP_PRIMARY_SYS_HOG("DGBU_RX", "dbgu_rxd", NULL),
+	PINMUX_MAP_PRIMARY_SYS_HOG("DGBU_TX", "dbgu_txd", NULL),
+};
+
+void usb_a9g20_init(void)
+{
+	pinmux_register_mappings(usb_a9g20_pinmux_map,
+				 ARRAY_SIZE(usb_a9g20_pinmux_map));
+}
+void sam9gm10g45ek_init(void)
+{
+	pinmux_register_mappings(sam9m10g45ek_pinmux_map,
+				 ARRAY_SIZE(sam9m10g45ek_pinmux_map));
+}
+
+static const struct of_device_id at91_board_of_match[] __initconst = {
+	{ .compatible = "atmel,at91sam9m10g45ek", .data = sam9gm10g45ek_init, },
+	{ .compatible = "calao,usb-a9g20", .data = usb_a9g20_init, },
+	{ /* sentinel */ }
+};
+
+static void __init early_dt_init(void)
+{
+	struct device_node *node;
+	const struct of_device_id *of_id;
+	void (*func)(void);
+
+	node = of_find_matching_node(NULL, at91_board_of_match);
+	if (node) {
+		of_id = of_match_node(at91_board_of_match, node);
+		func = of_id->data;
+		func();
+		of_node_put(node);
+	}
+}
 
 static void __init ek_init_early(void)
 {
 	/* Initialize processor: 12.000 MHz crystal */
 	at91_initialize(12000000);
+
+	early_dt_init();
 
 	/* DGBU on ttyS0. (Rx & Tx only) */
 	at91_register_uart(0, 0, 0);
@@ -76,6 +123,12 @@ static void __init at91_dt_device_init(void)
 
 	/* NAND */
 	ek_add_device_nand();
+
+	printk("0x%x\n", gpio_request(AT91_PIN_PB31, ""));
+
+	at91_set_A_periph(AT91_PIN_PB30, 0);
+
+	printk("0x%x\n", gpio_request(AT91_PIN_PB30, ""));
 }
 
 static const char *at91_dt_board_compat[] __initdata = {
