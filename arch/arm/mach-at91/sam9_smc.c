c5e21ce35d07f7d7303bb84e65e66a408ff9409d
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-11-27 19:29:57 +0800

    ARM: at91: add accessor to manage smc
    this will allow to configure the smc independtly of the register configuration
    as example on rm9200 different from sam9
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/sam9_smc.c
+++ arch/arm/mach-at91/sam9_smc.c
@@ -2,6 +2,7 @@
  * linux/arch/arm/mach-at91/sam9_smc.c
  *
  * Copyright (C) 2008 Andrew Victor
+ * Copyright (C) 2011 Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -17,13 +18,58 @@
 
 #include "sam9_smc.h"
 
-
 #define AT91_SMC_CS(id, n)	(smc_base_addr[id] + ((n) * 0x10))
 
 static void __iomem *smc_base_addr[2];
 
-static void __init sam9_smc_cs_configure(void __iomem *base, struct sam9_smc_config* config)
+static void __init_refok sam9_smc_cs_write_mode(void __iomem *base,
+					struct sam9_smc_config* config)
 {
+	__raw_writel(config->mode
+		   | AT91_SMC_TDF_(config->tdf_cycles),
+		   base + AT91_SMC_MODE);
+}
+
+int __init_refok sam9_smc_write_mode(int id, int cs,
+					struct sam9_smc_config* config)
+{
+	if (!smc_base_addr[id])
+		return -EINVAL;
+
+	sam9_smc_cs_write_mode(AT91_SMC_CS(id, cs), config);
+
+	return 0;
+}
+
+int __init_refok sam9_smc_configure(int id, int cs,
+					struct sam9_smc_config* config)
+{
+	if (!smc_base_addr[id])
+		return -EINVAL;
+
+	sam9_smc_cs_configure(AT91_SMC_CS(id, cs), config);
+
+	return 0;
+}
+
+static void __init_refok sam9_smc_cs_read_mode(void __iomem *base,
+					struct sam9_smc_config* config)
+{
+	u32 val = __raw_readl(base + AT91_SMC_MODE);
+
+	config->mode = (val & ~AT91_SMC_NWECYCLE);
+	config->tdf_cycles = (val & AT91_SMC_NWECYCLE) >> 16 ;
+}
+
+int __init_refok sam9_smc_read_mode(int id, int cs,
+					struct sam9_smc_config* config)
+{
+	void __iomen *base;
+
+	if (!smc_base_addr[id])
+		return -EINVAL;
+
+	base = AT91_SMC_CS(id, cs);
 
 	/* Setup register */
 	__raw_writel(AT91_SMC_NWESETUP_(config->nwe_setup)
@@ -45,14 +91,47 @@ static void __init sam9_smc_cs_configure(void __iomem *base, struct sam9_smc_con
 		   base + AT91_SMC_CYCLE);
 
 	/* Mode register */
-	__raw_writel(config->mode
-		   | AT91_SMC_TDF_(config->tdf_cycles),
-		   base + AT91_SMC_MODE);
+	sam9_smc_cs_write_mode(base, config);
+
+	return 0;
 }
 
-void __init sam9_smc_configure(int id, int cs, struct sam9_smc_config* config)
+int __init_refok sam9_smc_read(int id, int cs, struct sam9_smc_config* config)
 {
-	sam9_smc_cs_configure(AT91_SMC_CS(id, cs), config);
+	void __iomen *base;
+	u32 val;
+
+	if (!smc_base_addr[id])
+		return -EINVAL;
+
+	base = AT91_SMC_CS(id, cs);
+
+	/* Setup register */
+	val = __raw_readl(base + AT91_SMC_SETUP);
+
+	config->nwe_setup = val & AT91_SMC_NWESETUP;
+	config->ncs_write_setup = (val & AT91_SMC_NCS_WRSETUP) >> 8;
+	config->nrd_setup = (val & AT91_SMC_NRDSETUP) >> 16;
+	config->ncs_read_setup = (val & AT91_SMC_NCS_RDSETUP) >> 24;
+
+	/* Pulse register */
+	val = __raw_readl(base + AT91_SMC_PULSE);
+
+	config->nwe_setup = val & AT91_SMC_NWEPULSE;
+	config->ncs_write_pulse = (val & AT91_SMC_NCS_WRPULSE) >> 8;
+	config->nrd_pulse = (val & AT91_SMC_NRDPULSE) >> 16;
+	config->ncs_read_pulse = (val & AT91_SMC_NCS_RDPULSE) >> 24;
+
+	/* Cycle register */
+	val = __raw_readl(base + AT91_SMC_CYCLE);
+
+	config->write_cycle = val & AT91_SMC_NWECYCLE;
+	config->read_cycle = (val & AT91_SMC_NRDCYCLE) >> 16;
+
+	/* Mode register */
+	sam9_smc_cs_read_mode(base, config);
+
+	return 0;
 }
 
 void __init at91sam9_ioremap_smc(int id, u32 addr)
