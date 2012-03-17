ef6bcbc970a49b720b462496f992011730121ed1
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-03-17 19:16:15 +0800

    of: add helper for read u64 array
    Update the read u64 implementation to use it.
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -657,33 +657,40 @@ int of_property_read_u32_array(const struct device_node *np,
 EXPORT_SYMBOL_GPL(of_property_read_u32_array);
 
 /**
- * of_property_read_u64 - Find and read a 64 bit integer from a property
+ * of_property_read_u64_array - Find and read an array of 64 bit integers
+ * from a property.
+ *
  * @np:		device node from which the property value is to be read.
  * @propname:	name of the property to be searched.
  * @out_value:	pointer to return value, modified only if return value is 0.
  *
- * Search for a property in a device node and read a 64-bit value from
+ * Search for a property in a device node and read 64-bit value(s) from
  * it. Returns 0 on success, -EINVAL if the property does not exist,
  * -ENODATA if property does not have a value, and -EOVERFLOW if the
  * property data isn't large enough.
  *
  * The out_value is modified only if a valid u64 value can be decoded.
  */
-int of_property_read_u64(const struct device_node *np, const char *propname,
-			 u64 *out_value)
+int of_property_read_u64_array(const struct device_node *np,
+			       const char *propname, u64 *out_values,
+			       size_t sz)
 {
 	struct property *prop = of_find_property(np, propname, NULL);
+	const __be64 *val;
 
 	if (!prop)
 		return -EINVAL;
 	if (!prop->value)
 		return -ENODATA;
-	if (sizeof(*out_value) > prop->length)
+	if ((sz * sizeof(*out_values)) > prop->length)
 		return -EOVERFLOW;
-	*out_value = of_read_number(prop->value, 2);
+
+	val = prop->value;
+	while (sz--)
+		*out_values++ = of_read_number(val++, 2);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(of_property_read_u64);
+EXPORT_SYMBOL_GPL(of_property_read_u64_array);
 
 /**
  * of_property_read_string - Find and read a string from a property
